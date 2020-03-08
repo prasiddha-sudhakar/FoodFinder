de13f94e817cfab77aa9a0d72019587b5c66dc1d
prasiddha-sudhakar <44575137+prasiddha-sudhakar@users.noreply.github.com>
2020-03-08 09:31:59 -0400

    Latest changes

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const _ = require("lodash");
+//const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(2020, function() {
+  console.log("Server started on port 2020");
+});
